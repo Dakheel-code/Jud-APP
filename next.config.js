4ca16c87c5f277e49bb38c857e2e257e29a29c9b
Dakheel-code <Dakheel@jud.sa>
2025-12-24 01:54:24 +0300

    Fix build error: remove babel-loader webpack config

--- next.config.js
+++ next.config.js
@@ -4,26 +4,10 @@ const nextConfig = {
   images: {
     domains: ['jud.sa'],
   },
-  experimental: {
-    serverActions: true,
-  },
   i18n: {
     locales: ['ar'],
     defaultLocale: 'ar',
   },
-  // ضمان UTF-8 encoding
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'],
-        },
-      },
-    });
-    return config;
-  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
