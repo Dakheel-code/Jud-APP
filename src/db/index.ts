996ba37428e21d9f8d935171950d4fa089170e9b
Dakheel-code <Dakheel@jud.sa>
2025-12-24 00:03:58 +0300

    Fix Supabase pooler connection with SSL and prepare:false

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,5 +4,11 @@ import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, {
+  prepare: false,
+  ssl: 'require',
+  connection: {
+    application_name: 'jud-app'
+  }
+});
 export const db = drizzle(client, { schema });
